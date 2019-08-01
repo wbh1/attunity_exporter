55df462b0122663cb7ace78bbd196a8783d0d030
Will Hegedus <wbhegedus@liberty.edu>
2019-08-01 18:29:00 -0400

    More work on filtering tasks

--- collector/attunity_collector.go
+++ collector/attunity_collector.go
@@ -51,16 +51,31 @@ var (
 		[]string{"server", "task"},
 		nil,
 	)
+
+	taskSourceLatencyDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, "task", "source_latency_seconds"),
+		"Source latency for this task",
+		[]string{"server", "task"},
+		nil,
+	)
 )
 
 // Config is the destination type for when the config file gets
 // unmarshalled by the YAML package
 type Config struct {
-	Server   string   `yaml:"server"`
-	Username string   `yaml:"user"`
-	Password string   `yaml:"password"`
-	Timeout  int      `yaml:"timeout,omitempty"`
-	Tasks    []string `yaml:"tasks,omitempty"`
+	Server        string         `yaml:"server"`
+	Username      string         `yaml:"user"`
+	Password      string         `yaml:"password"`
+	Timeout       int            `yaml:"timeout,omitempty"`
+	IncludedTasks []*TasksFilter `yaml:"included_tasks,omitempty"`
+	ExcludedTasks []*TasksFilter `yaml:"excluded_tasks,omitempty"`
+}
+
+// TasksFilter is used in the config.yml to specify a set of tasks on a server that
+// should be excluded or included.
+type TasksFilter struct {
+	Server string   `yaml:"server"`
+	Tasks  []string `yaml:"tasks"`
 }
 
 // AttunityCollector implements the Prometheus Collector interface.
@@ -76,7 +91,14 @@ type AttunityCollector struct {
 	SessionID string
 
 	// A list of tasks to get details on
-	Tasks []string
+	// If empty, get all tasks.
+	// The key is server name.
+	IncludedTasks []map[string][]string
+
+	// A list of tasks to NOT get details on
+	// Takes precedence over IncludedTasks
+	// The key is server name.
+	ExcludedTasks []map[string][]string
 
 	// How long to wait for API responses; 0=infinite
 	Timeout int
@@ -137,11 +159,29 @@ func NewAttunityCollector(cfg *Config) *AttunityCollector {
 	}
 	sessionID := resp.Header.Get("Enterprisemanager.apisessionid")
 
+	incl := make([]map[string][]string, 1)
+	for i := 0; i < len(cfg.IncludedTasks); i++ {
+		for _, t := range cfg.IncludedTasks[i].Tasks {
+			incl[i] = make(map[string][]string)
+			incl[i][cfg.IncludedTasks[i].Server] = append(incl[i][cfg.IncludedTasks[i].Server], t)
+
+		}
+	}
+
+	excl := make([]map[string][]string, 1)
+	for i := 0; i < len(cfg.ExcludedTasks); i++ {
+		for _, t := range cfg.ExcludedTasks[i].Tasks {
+			excl[i] = make(map[string][]string)
+			excl[i][cfg.ExcludedTasks[i].Server] = append(excl[i][cfg.ExcludedTasks[i].Server], t)
+		}
+	}
+
 	return &AttunityCollector{
-		APIURL:     apiURL,
-		SessionID:  sessionID,
-		Tasks:      cfg.Tasks,
-		httpClient: client,
+		APIURL:        apiURL,
+		SessionID:     sessionID,
+		IncludedTasks: incl,
+		ExcludedTasks: excl,
+		httpClient:    client,
 	}
 
 }
@@ -248,8 +288,83 @@ func (a *AttunityCollector) taskStates(server string) ([]task, error) {
 		return nil, err
 	}
 
+	exclIndex := -1
+	for x, y := range a.ExcludedTasks {
+		if _, ok := y[server]; ok {
+			exclIndex = x
+			logrus.Debugf("Found index for %s %v", server, exclIndex)
+			break
+		}
+	}
+
+	inclIndex := -1
+	for x, y := range a.IncludedTasks {
+		if _, ok := y[server]; ok {
+			inclIndex = x
+			logrus.Debugf("Found index for %s %v", server, inclIndex)
+			break
+		}
+	}
+
+	if exclIndex >= 0 || inclIndex >= 0 {
+		var filtered []task
+		// Filter to EXCLUDE specific tasks
+		if len(a.ExcludedTasks[exclIndex][server]) > 0 && len(a.IncludedTasks[inclIndex][server]) > 0 {
+			filtered = a.filterIncluded(&tl.Items, a.IncludedTasks[inclIndex][server])
+			filtered = a.filterExcluded(&filtered, a.ExcludedTasks[exclIndex][server])
+		} else if len(a.ExcludedTasks[exclIndex][server]) > 0 {
+			filtered = a.filterExcluded(&tl.Items, a.ExcludedTasks[exclIndex][server])
+		} else if len(a.IncludedTasks[inclIndex][server]) > 0 {
+			filtered = a.filterIncluded(&tl.Items, a.IncludedTasks[inclIndex][server])
+		}
+
+		return filtered, nil
+	}
+
 	return tl.Items, nil
 
+}
+
+func (a *AttunityCollector) filterExcluded(tl *[]task, excluded []string) []task {
+	filtered := *tl
+	length := len(*tl)
+
+	for _, t1 := range excluded {
+		matched := false
+		for i, t2 := range *tl {
+			if t1 == t2.Name {
+				matched = true
+				if i < length {
+					filtered = append(filtered[:i], filtered[i+1:]...)
+				} else {
+					filtered = filtered[:i]
+				}
+				length--
+			}
+		}
+		if !matched {
+			logrus.Error("No task found matching: ", t1)
+		}
+	}
+	return filtered[:length]
+}
+
+func (a *AttunityCollector) filterIncluded(tl *[]task, included []string) []task {
+	var filtered []task
+	for _, t1 := range included {
+		matched := false
+		for _, t2 := range *tl {
+			if t1 == t2.Name {
+				matched = true
+				filtered = append(filtered, t2)
+				break
+			}
+		}
+		if !matched {
+			logrus.Error("No task found matching: ", t1)
+		}
+	}
+	return filtered
 }
 
 func (t task) details(server string, a *AttunityCollector, ch chan<- prometheus.Metric) {
@@ -285,26 +400,42 @@ func (t task) details(server string, a *AttunityCollector, ch chan<- prometheus.
 		return
 	}
 
-	times := strings.Split(td.Item.CDCLatency.TotalLatency, ":")
+	tl, err := latency(td.Item.CDCLatency.TotalLatency)
+	if err != nil {
+		logrus.Error(err)
+	} else {
+		ch <- prometheus.MustNewConstMetric(taskTotalLatencyDesc, prometheus.GaugeValue, tl.Seconds(), server, t.Name)
+	}
+
+	sl, err := latency(td.Item.CDCLatency.SourceLatency)
+	if err != nil {
+		logrus.Error(err)
+	} else {
+		ch <- prometheus.MustNewConstMetric(taskSourceLatencyDesc, prometheus.GaugeValue, sl.Seconds(), server, t.Name)
+	}
+
+}
+
+func latency(hhmmss string) (time.Duration, error) {
+	times := strings.Split(hhmmss, ":")
 	hoursInt, err := strconv.Atoi(times[0])
 	if err != nil {
 		logrus.Error(err)
-		return
+		return time.Second, err
 	}
 	minsInt, err := strconv.Atoi(times[1])
 	if err != nil {
 		logrus.Error(err)
-		return
+		return time.Second, err
 	}
 	secsInt, err := strconv.Atoi(times[2])
 	if err != nil {
 		logrus.Error(err)
-		return
+		return time.Second, err
 	}
 	latency := (time.Duration(hoursInt) * time.Hour) + (time.Duration(minsInt) * time.Minute) + (time.Duration(secsInt) * time.Second)
 
-	ch <- prometheus.MustNewConstMetric(taskTotalLatencyDesc, prometheus.GaugeValue, latency.Seconds(), server, t.Name)
-
+	return latency, nil
 }
 
 func (a *AttunityCollector) newAPIRequest(path string) (req *http.Request, err error) {

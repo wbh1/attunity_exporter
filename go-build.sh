d76637e6060a0d166b5ec5c793ea9a43b23911c1
Sriram <sriram.ramchandra@avalara.com>
2020-04-17 13:45:52 -0700

    Go build for diff platforms.

---
+++ go-build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+package_name="attunity_exporter"
+git_sha=$(git rev-parse --short HEAD)
+version=${TRAVIS_TAG:-$git_sha}
+
+
+platforms=("windows/amd64" "darwin/amd64" "linux/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -v -o $output_name $package
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
